f2f067dd4dcd2a611fa4a4dbfd90e3be216afae0
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-12-01 17:47:45 +0000

    Add on delete cascade

--- db/album_manager.sql
+++ db/album_manager.sql
@@ -12,5 +12,6 @@ CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     genre VARCHAR(255),
-    artist_id INT REFERENCES artists(id)
+    -- ON DELETE CASCADE - Deletes from album and from artist
+    artist_id INT REFERENCES artists(id) ON DELETE CASCADE
 );
